894bdaa54a0c68e03f04bb3abebbe8fae483cd58
Abhilash Upadhya <abhilashupadhya@Abhilashs-MacBook-Pro.local>
2017-09-22 22:18:14 -0400

    Replica comments

--- replica.py
+++ replica.py
@@ -80,85 +80,105 @@ class Replica:
 
     def handle_new_request(self, request):
         # request is a tuple of client and operation
+        # Validate client
+        if not valid_client():
+            return
+        
+        # Send request to head along the chain
         send(request, to=self.previous)
-        # init_timer
+        
+        # Initialize and start timer
         timer = self.timer.new_timer()
         timer.start()
-        # -- receive result shuttle ack
+        -- receive result shuttle ack
         client, operation = request.client, request.operation
-        # blocking_wait => valid result
+        
+        # Wait untill result shuttles comes back from tail or timer expires
+        # Send 'reconfigure-request' to olympus if timer expires
         await(timer.timed_out() or ResultShuttle((client, operation), _) in received)
         if timer.timed_out():
-            # TODO: Handle error case
             reconfigure_request = ReconfigureRequest()
             send(reconfigure_request, to=self.olympus)
             return
-
-        # cancel_timer on valid result          # TODO: error case
         timer.stop()
 
-    def receive_request_shuttle(self, request):
-        client = request.client
-        # verify client
-        operation = request.operation
-        # return result if <client_id, operation, result> in cache
-        # common
-        # verify order_proof
+    def receive_request_shuttle(self, request_shuttle):
+        # request is a shuttle containing orderproof and resultproofs
+        # Extract client ID, operation, and request ID from orderproof
+        client = request_shuttle.order_proof.client
+        operation = request_shuttle.order_proof.operation
+        request = request_shuttle.order_proof.operation
+        
+        # Validate received order_proof in shuttle
         verification_failed = verify_order_proof(request)
         if verification_failed:
             reconfigure_request = ReconfigureRequest()
             send(reconfigure_request, to=self.olympus)
             return
+
+        # Apply operation to the running state and get result
         self.running_state, result = self.running_state(operation)
-        # add sign(order_statement) to order_proof
+
+        # Create order_proof
+        order_statement = OrderStatement(request, slot, operation)
+        signed_order_statement = sign(order_statement)
+        order_proof = OrderProof(request, slot, operation, self.configuration_id, [signed_order_statement])
+        
         # add order_proof to history
+        self.history.append(order_proof)
+
+        # cache client ID and operation
         self.cache[(client, operation)] = None
+        
         # add sign(result_statement) to result_proof
         result_statement = ResultStatement(request, slot, operation)
         signed_result_statement = sign(result_statement)
         result_statements = request.order_proof.orderstatments + [signed_result_statement]
         result_proof = ResultProof(request, slot, operation, self.configuration_id, result_statements)
-        # forward Shuttle(<order_proof, result_proof>)
+        
+        # forward Shuttle to the next replica
         shuttle = Shuttle(request, order_proof, result_proof)
         send(shuttle, to=self.next)
-        # cache <client_id, operation, _>
-        self.cache[(client, operation)] = None
+        
+        # Initialize and start timer
         timer = self.timer.new_timer()
         timer.start()
-        # -- receive result shuttle ack
-        # blocking_wait => valid result
+        -- receive result shuttle ack
+        
+        # Wait until result shuttle comes back from client or timer expires
         await(timer.timed_out() or ResultShuttle((client, operation), _) in received)
+
+        # Send 'reconfigure-request' to Olympus if timed out
         if timer.timed_out():
-            # TODO: Handle error case
-            # verify order_proof
             reconfigure_request = ReconfigureRequest()
             send(reconfigure_request, to=self.olympus)
             return
-        # cancel_timer on valid result
         timer.stop()
 
-    # TODO: consistency
-    def receive_result_shuttle(self, result):
-        # verify, Digital Signature verification
-        client, operation = result.client, result.operation
-        # cache <client_id, operation, result>
-        self.cache[(client, operation)] = result
+
+    def receive_result_shuttle(self, result_shuttle):
+        # Perform Signature verification
+        client, operation = result_shuttle.client, result_shuttle.operation
+        
+        # cache client_id, operation and result
+        self.cache[(client, operation)] = result_shuttle.result
         # forward result_shuttle to previous
 
         # send <result, result_proof> to client
-        # result, result_shuttle = get result shuttle and result from cache
-        # TODO: Handle this in receive handlers
         send((result, result_proof), request.client)
-        # forward result_proof ack to previous if any
+        
+        # forward result_shuttle to previous replica
         send(result_shuttle, self.previous)
 
     def receive_wedge_request(self, request):
         # Create a new wedge statement
         wedgeStatement = ('wedged', self.id, self.running_state, self.history)
+        
         #Send the statement to Olympus
         send(wedgeStatement,to=self.olympus)
+        
         #set the replica state to Immutable
-        self.mode = 'IMMUTABLE'
+        self.mode = Mode.IMMUTABLE
 
 
     def receive_catchup_messges(self, request):
@@ -167,14 +187,18 @@ class Replica:
         #send running state message to Olympus
         send(self.running_state,to=olympus)
 
-    def receive_checkpoint_request(self, request):
-        request[self.id] = (hash(self.running_state), checkpoint_slot)
-        send(request, to=self.next)
+    def receive_checkpoint_request(self, checkpoint_shuttle):
+        # Replica hashes its running state and adds it to checkpoint shuttle
+        # Send shuttle to the next replica
+        checkpoint_shuttle[self.id] = (hash(self.running_state), checkpoint_slot)
+        send(checkpoint_shuttle, to=self.next)
 
 
-    def receive_checkpoint_result(self, result):
-        result_checkpoint = result[self.id]
+    def receive_checkpoint_result(self, result_checkpoint_shuttle):
+        result_checkpoint = result_checkpoint_shuttle[self.id]
+        # If replica's history is greater than the checkpoint slot,
+        # truncate history and remove checkpoint entry from the shuttle
         if result_checkpoint[checkpoint_slot] >= len(self.history):
             self.history = self.history[-checkpoint_slot:]
-            del result[self.id]
-        send(result, to=self.previous)
+            del result_checkpoint_shuttle[self.id]
+        send(result_checkpoint_shuttle, to=self.previous)
