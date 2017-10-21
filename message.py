c4542fc6bde3b02f4b84ccb17bf02606e44dc83e
Abhishek S P <abhisheksp1993@gmail.com>
2017-10-20 21:14:39 -0400

    [SP] [Refactor] Replace use named tuples for order/result stmt and proofs

--- message.py
+++ message.py
@@ -8,12 +8,18 @@ from collections import namedtuple
 class Message:
     client_request = 'request'
     replica_response = 'response'
-    request_shuttle = 'request_shuttle'
+    request_shuttle_tag = 'request_shuttle'
     OrderStatement = namedtuple('OrderStatement', 'slot operation replica_id')
     OrderProof = namedtuple(
         'OrderProof',
         'client_id request_id slot operation configuration order_statements'
     )
+    ResultStatement = namedtuple('OrderStatement', 'result operation replica_id')
+    ResultProof = namedtuple(
+        'ResultProof',
+        'client_id request_id result operation configuration result_statements'
+    )
+    RequestShuttle = namedtuple('RequestShuttle', 'order_proof result_proof')
 
     def __init__(self, identifier):
         self.identifier = identifier
@@ -30,25 +36,26 @@ class Message:
 
     def new_request_shuttle(self, payload):
         message_body = {'replica_id': self.identifier, 'payload': payload}
-        return self.request_shuttle, message_body
+        return self.request_shuttle_tag, message_body
 
-    def new_order_statement(self, previous_order_statement):
+    def new_order_statement(self, operation, slot=None):
+        slot = slot if slot else uuid.uuid4()
         order_statement = {
-            'slot': previous_order_statement.get('slot', uuid.uuid4()),
-            'operation': previous_order_statement['operation'],
+            'slot': slot,
+            'operation': operation,
             'replica_id': self.identifier
         }
         return order_statement
 
     def new_order_proof(self, previous_order_proof, new_order_statement):
-        order_statements = previous_order_proof['order_statements'] + [new_order_statement]
+        order_statements = previous_order_proof.order_statements + [new_order_statement]
 
         # TODO: figure out serializing named tuple
         order_proof = {
-            'client_id': previous_order_proof['client_id'],
-            'request_id': previous_order_proof['request_id'],
-            'slot': previous_order_proof['slot'],
-            'operation': previous_order_proof['operation'],
+            'client_id': previous_order_proof.client_id,
+            'request_id': previous_order_proof.request_id,
+            'slot': previous_order_proof.slot,
+            'operation': previous_order_proof.operation,
             'configuration': 'DEFAULT',
             'order_statements': order_statements
         }
@@ -57,21 +64,21 @@ class Message:
     def new_result_statement(self, operation, result):
         hashed_result = nacl.hash.sha256(bytes(result, 'utf-8'), encoder=nacl.encoding.HexEncoder)
         result_statement = {
-            'operation': operation,
             'result': hashed_result,
+            'operation': operation,
             'replica_id': self.identifier
         }
         return result_statement
 
     def new_result_proof(self, previous_result_proof, new_result_statement):
-        result_statements = previous_result_proof['result_statements'] + [new_result_statement]
+        result_statements = previous_result_proof.result_statements + [new_result_statement]
 
         # TODO: figure out serializing named tuple
         result_proof = {
-            'client_id': previous_result_proof['client_id'],
-            'request_id': previous_result_proof['request_id'],
-            'result': previous_result_proof['result'],
-            'operation': previous_result_proof['operation'],
+            'client_id': previous_result_proof.client_id,
+            'request_id': previous_result_proof.request_id,
+            'result': previous_result_proof.result,
+            'operation': previous_result_proof.operation,
             'configuration': 'DEFAULT',
             'result_statements': result_statements
         }
