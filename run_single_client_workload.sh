ee9e622acf7c6d6cd32edf5332db3280727e9cf0
Abhishek S P <abhisheksp1993@gmail.com>
2017-10-22 09:36:15 -0400

    [SP] Send Replica processes to Client

--- run_single_client_workload.sh
+++ run_single_client_workload.sh
@@ -6,4 +6,4 @@ python -m da.compiler client.da
 python -m da.compiler replica.da
 python -m da.compiler olympus.da
 python -m da.compiler main.da
-python -m da -n MainNode --master  --message-buffer-size 1000000 main.da
+python -m da -n MainNode --master  --message-buffer-size 1000000 main.da -c single_client_workload.txt
