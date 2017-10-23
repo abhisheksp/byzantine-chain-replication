4a492c756661a554980ab50f7079dc92430f99eb
Abhishek S P <abhisheksp1993@gmail.com>
2017-10-22 22:26:08 -0400

    [SP] Increase message size for client run script

--- run_client.sh
+++ run_client.sh
@@ -7,4 +7,4 @@ python -m da.compiler client.da
 python -m da.compiler replica.da
 python -m da.compiler olympus.da
 python -m da.compiler main.da
-python -m da -n ClientNode --logfile --logfilename ../logs/client.log --logfilelevel info -D main.da
+python -m da -n ClientNode --logfile --logfilename ../logs/client.log --logfilelevel info -D --message-buffer-size 100000 main.da
