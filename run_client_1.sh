d64b468098e8c57caccc51d1ee097a59fadeafd3
Abhishek S P <abhisheksp1993@gmail.com>
2017-10-22 20:44:59 -0400

    [SP] Test Case 1 with client logging

---
+++ run_client_1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source ~/Environments/async36/bin/activate
+rm logs/client.log
+cd src
+python -m da.compiler client.da
+python -m da.compiler replica.da
+python -m da.compiler olympus.da
+python -m da.compiler main.da
+python -m da -n ClientNode --logfile --logfilename ../logs/client.log --logfilelevel info -D main.da
