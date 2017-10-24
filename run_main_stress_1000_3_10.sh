6c3c0aea8cd8ce7fc4aca8691741a273683c82bc
Abhilash Upadhya <abhilashupadhya@Abhilashs-MacBook-Pro.local>
2017-10-23 23:45:30 -0400

    [Abhilash] last commit before Phase 2 submission

--- run_main_stress_1000_3_10.sh
+++ run_main_stress_1000_3_10.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source compile.sh
-python -m da -n MainNode --master  --logfile --logfilename ../logs/main.log --logfilelevel info --message-buffer-size 1000000 main.da -c stress_100.txt
+python -m da -n MainNode --master  --logfile --logfilename ../logs/main.log --logfilelevel info --message-buffer-size 1000000 main.da -c stress_1000_7_10.txt
--- run_main_stress_2000_11_1.sh
+++ run_main_stress_2000_11_1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source compile.sh
-python -m da -n MainNode --master  --logfile --logfilename ../logs/main.log --logfilelevel info --message-buffer-size 100000000 main.da -c stress_10000_11_1.txt
+python -m da -n MainNode --master  --logfile --logfilename ../logs/main.log --logfilelevel info --message-buffer-size 100000000 main.da -c stress_2000_11_1.txt
