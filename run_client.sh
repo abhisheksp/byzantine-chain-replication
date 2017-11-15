04eb0e34d9a8f0e28994a3e3b83355c26f1790ff
Abhishek S P <abhisheksp1993@gmail.com>
2017-11-15 13:25:48 -0500

    [SP] Parameterize current configuration and rename 'new_configuration' response tag

--- run_client.sh
+++ run_client.sh
@@ -5,4 +5,4 @@ rm logs/client.log
 echo 'Compiling DistAlgo files...'
 source compile.sh
 export IP_ADDR=`/sbin/ifconfig docker0 |grep 'inet '| cut -d: -f2 | awk '{print $2}'`
-python -m da -n ClientNode --logfile --logfilename ../logs/client.log --logfilelevel debug -D --message-buffer-size 100000 --hostname $IP_ADDR main.da
+python -m da -n ClientNode --logfile --logfilename ../logs/client.log --logfilelevel info -D --message-buffer-size 100000 --hostname $IP_ADDR main.da
