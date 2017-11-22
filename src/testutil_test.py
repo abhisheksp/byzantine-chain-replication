a82735be4dbbc2e91d9441d104ecf78429294de5
Abhishek S P <abhisheksp1993@gmail.com>
2017-11-21 22:16:09 -0500

    [SP] Upadate pending requests only if response is valid at Client and Minor fixes

--- src/testutil_test.py
+++ src/testutil_test.py
@@ -13,18 +13,19 @@ class TestUtilTest(unittest.TestCase):
         temp_file_path = path.join(test_dir, 'test_config.txt')
         with open(temp_file_path, 'w') as f:
             f.write('failures[0,0] = client_request(2,1), crash(); shuttle(1,3),sleep(5)\n' +
-                    'failures[1,2] = result_shuttle(0,1),drop(); shuttle(1,3),omit_send()\n' +
+                    'failures[1,2] = result_shuttle(0,1),drop(); shuttle(1,3),drop()\n' +
                     'failures[1,1] = wedge_request(2), truncate_history(3)'
                     )
 
         config = util.read_configuration(temp_file_path)
-        print(config)
-        print(testutil.parse_failure_scenarios(config))
+        actual = testutil.parse_failure_scenarios(config)
         expected = {}
         expected[1, 1] = {(3, 2, 'client_request', 'crash'): None, (2, 4, 'shuttle', 'sleep'): 5}
-        expected[2, 3] = {(1, 2, 'result_shuttle', 'drop'): None, (2, 4, 'shuttle', 'omit_send'): None}
+        expected[2, 3] = {(1, 2, 'result_shuttle', 'drop'): None, (2, 4, 'shuttle', 'drop'): None}
         expected[2, 2] = {(3, 'wedge_request', 'truncate_history'): 3}
         shutil.rmtree(test_dir)
+
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
