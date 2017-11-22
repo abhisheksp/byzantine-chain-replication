fe2d632d843961a0fbd1c8856074a33f54ae4e5d
Abhishek S P <abhisheksp1993@gmail.com>
2017-11-22 15:35:30 -0500

    [SP] Add perform900 test

---
+++ run_main_perform900.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo 'Cleaning Existing Replica Containers...'
+source clean_containers_images.sh
+echo 'Building new Replica Image'
+docker build -t replicanode_img .
+echo 'Deleting old log file: logs/main.log'
+rm logs/main.log
+echo 'Deleting old log file: logs/replica.log'
+rm logs/replica.log
+echo 'Compiling DistAlgo files...'
+source compile.sh
+export IP_ADDR=`/sbin/ifconfig docker0 |grep 'inet '| cut -d: -f2 | awk '{print $2}'`
+python -m da -n MainNode --logfile --logfilename ../logs/main.log --logfilelevel info --message-buffer-size 100000 --hostname $IP_ADDR main.da -c perform900.txt
