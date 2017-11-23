985d545ae733a3c9ae5597223b3a385ffd5d8148
Abhishek S P <abhisheksp1993@gmail.com>
2017-11-22 19:41:12 -0500

    [SP] Remove State JSON files

--- run_main_1.sh
+++ run_main_1.sh
@@ -8,6 +8,9 @@ echo 'Deleting old log file: logs/main.log'
 rm logs/main.log
 echo 'Deleting old log file: logs/replica.log'
 rm logs/replica.log
+echo 'Deleting old persisted states'
+rm logs/replica_state_*
+rm logs/client_state_*
 echo 'Compiling DistAlgo files...'
 source compile.sh
 export IP_ADDR=`/sbin/ifconfig docker0 |grep 'inet '| cut -d: -f2 | awk '{print $2}'`
